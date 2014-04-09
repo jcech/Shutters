c41c8f278fbb49c4007c23c1605d1d6f90462137
John Cech & Ben Birdsall <student@epicodus.com>
2014-04-09 11:39:08 -0700

    adds photo CRUD functionality via Paperclip

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,38 @@
 class PhotosController < ApplicationController
+
+  before_filter :authorize, only: [:new, :show]
+
   def index
     @photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to root_url, notice: "Photo deleted!"
+  end
+
+  def create
+    @photo = Photo.new(user_params)
+    if @photo.save
+      redirect_to root_url, notice: "Nice Photo!"
+    else
+      flash.now.alert = "No photo Selected"
+      render('new')
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:photo).permit(:user_id, :image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409160635) do
+ActiveRecord::Schema.define(version: 20140409173809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "photos", force: true do |t|
-    t.integer "user_id"
+    t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "sessions", force: true do |t|
